315ca5d596f3d2efebe1fb9bad6733b9772e529c
Zomzog <Zomzog@gmail.com>
2017-04-20 21:36:02 +0200

    bdd clean

--- auth-service/src/main/java/bzh/zomzog/zeshop/auth/configuration/DatabaseConfiguration.java
+++ auth-service/src/main/java/bzh/zomzog/zeshop/auth/configuration/DatabaseConfiguration.java
@@ -2,18 +2,13 @@ package bzh.zomzog.zeshop.auth.configuration;
 
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
 import org.h2.server.web.WebServlet;
 import org.h2.tools.Server;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-
-import liquibase.integration.spring.SpringLiquibase;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -34,21 +29,6 @@ public class DatabaseConfiguration {
         final ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
-    }
-
-    @Bean
-    public SpringLiquibase liquibase(final DataSource dataSource, final LiquibaseProperties liquibaseProperties) {
-
-        // Use liquibase.integration.spring.SpringLiquibase if you don't want
-        // Liquibase to start asynchronously
-        final SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
-        liquibase.setContexts(liquibaseProperties.getContexts());
-        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
-        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-        liquibase.setShouldRun(liquibaseProperties.isEnabled());
-        return liquibase;
     }
 
     @Bean
