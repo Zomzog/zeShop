41a18a0651a64b46f29a16b5a098ec325aca4816
Zomzog <zomzog@gmail.com>
2017-07-22 00:24:03 +0200

    Fix ProductMapper

--- product-service/src/main/java/bzh/zomzog/zeshop/product/service/mapper/product/ProductMapper.java
+++ product-service/src/main/java/bzh/zomzog/zeshop/product/service/mapper/product/ProductMapper.java
@@ -4,11 +4,8 @@ import bzh.zomzog.zeshop.product.domain.Product;
 import bzh.zomzog.zeshop.product.service.dto.product.ProductDTO;
 import bzh.zomzog.zeshop.product.service.mapper.ImageMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
-
-import java.util.List;
 
 /**
  * Mapper for the entity Product and its DTO ProductDTO.
@@ -18,19 +15,11 @@ public interface ProductMapper {
 
     ProductDTO productToProductDTO(Product product);
 
-    List<ProductDTO> productsToProductDTOs(List<Product> products);
-
-    @Mappings({ //
-            @Mapping(target = "createdDate", ignore = true), //
-            @Mapping(target = "updatedDate", ignore = true), //
+    @Mappings({
     })
     Product productDTOToProduct(ProductDTO productDTO);
 
-    List<Product> productDTOsToProducts(List<ProductDTO> productDTOs);
-
-    @Mappings({ //
-            @Mapping(target = "createdDate", ignore = true), //
-            @Mapping(target = "updatedDate", ignore = true), //
+    @Mappings({
     })
     Product update(ProductDTO productDTO, @MappingTarget Product product);
 }
